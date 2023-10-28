c893a295812a1d2a1cc0f6fd623ef6a5d43a8572
Hisham Asghar <hisham@octacer.com>
2023-10-28 09:34:22 +0100

    Import flavors setup from very_good client

--- android/app/src/main/kotlin/com/example/my_shop_ecommerce_flutter/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.my_shop_ecommerce_flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ lib/main_production.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:my_shop_ecommerce_flutter/src/platform/platform_is.dart';
+import 'package:my_shop_ecommerce_flutter/src/services/auth_service.dart';
+import 'package:my_shop_ecommerce_flutter/src/utils/provider_logger.dart';
+
+import 'src/app.dart';
+
+void main() async {
+  // TODO: Uncomment this when implementing Stripe payments
+  // TODO: Add platform checks to disable on desktop
+  WidgetsFlutterBinding.ensureInitialized();
+  if (PlatformIs.iOS || PlatformIs.android) {
+    // TODO: Provide key
+    //Stripe.publishableKey = stripePublishableKey;
+    // Stripe.merchantIdentifier = 'merchant.flutter.stripe.test';
+    // Stripe.urlScheme = 'flutterstripe';
+    // await Stripe.instance.applySettings();
+  }
+
+  final authService = MockAuthService();
+  if (authService.currentUser == null) {
+    authService.signInAnonymously();
+  }
+  runApp(ProviderScope(
+    overrides: [
+      authServiceProvider.overrideWithValue(authService),
+    ],
+    observers: [ProviderLogger()],
+    child: MyApp(),
+  ));
+}
