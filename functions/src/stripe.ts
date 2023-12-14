5c0169fa259a12237c2993a109bf45c2207c7b5a
tagheir@gmail.com <Hisham Asghar>
2023-12-14 14:24:38 +0000

    Attempt to get webhook signature verification working. Fail (#99)

--- functions/src/stripe.ts
+++ functions/src/stripe.ts
@@ -77,70 +77,71 @@ export async function stripeWebhook(req: functions.https.Request, res: functions
         console.error(`⚠️  Stripe webhook secret is not set`)
         res.sendStatus(400)
     }
-    const event: Stripe.Event = req.body;
+    const event: Stripe.Event = req.body
 
     // TODO: Enable signature verification
     // Retrieve the event by verifying the signature using the raw body and secret.
-    //let event: Stripe.Event;
-  
-    //const stripe = getStripe()
-    //console.log('webhook:', req);
+    // let event: Stripe.Event;
+    // const stripe = getStripe()
+    // console.log('webhook body:', req.body)
+    // console.log('webhook headers:', req.headers['stripe-signature'])
+    // console.log('webhook secret:', stripeWebhookSecretKey)
     // try {
+    //     // https://codeburst.io/express-js-on-cloud-functions-for-firebase-86ed26f9144c
     //     // https://dev.to/ting682/e-commerce-payments-using-firebase-nodejs-and-square-api-40jn
     //     // https://stackoverflow.com/questions/70159949/webhook-signature-verification-failed-with-express-stripe
     //     event = stripe.webhooks.constructEvent(
     //       req.body, // TODO: need body parser?
     //       req.headers['stripe-signature'] || [],
     //       stripeWebhookSecretKey
-    //     );
+    //     )
     // } catch (err) {
-    //     console.log(`⚠️  Webhook signature verification failed.`);
-    //     res.sendStatus(400);
-    //     return;
+    //     console.log(`⚠️  Webhook signature verification failed.`)
+    //     res.sendStatus(400)
+    //     return
     // }
-  
     // Extract the data from the event.
-    const data: Stripe.Event.Data = event.data;
-    const eventType: string = event.type;
+    const data: Stripe.Event.Data = event.data
+    const eventType: string = event.type
 
     if (eventType === 'payment_intent.succeeded') {
         // Cast the event into a PaymentIntent to make use of the types.
-        const pi: Stripe.PaymentIntent = data.object as Stripe.PaymentIntent;
+        const pi: Stripe.PaymentIntent = data.object as Stripe.PaymentIntent
   
         // Funds have been captured
         // Fulfill any orders, e-mail receipts, etc
         // To cancel the payment after capture you will need to issue a Refund (https://stripe.com/docs/api/refunds).
-        console.log(`🔔  Webhook received: ${pi.object} ${pi.status}!`);
-        console.log(`💰 Payment captured! Amount: ${pi.amount}, customerId: ${pi.customer}`);
+        console.log(`🔔  Webhook received: ${pi.object} ${pi.status}!`)
+        console.log(`💰 Payment captured! Amount: ${pi.amount}, customerId: ${pi.customer}`)
         
         // Order fullfillment
         try {
             await fullfillOrder(pi)
-            console.log('🔔 Order fullfillment succeeded.');
+            console.log('🔔 Order fullfillment succeeded.')
         } catch (e) {
-            console.log(`❌ Order fullfillment failed: ${e}`);
+            console.log(`❌ Order fullfillment failed: ${e}`)
         }
     }
     if (eventType === 'payment_intent.payment_failed') {
         // Cast the event into a PaymentIntent to make use of the types.
-        const pi: Stripe.PaymentIntent = data.object as Stripe.PaymentIntent;
-        console.log(`🔔  Webhook received: ${pi.object} ${pi.status}!`);
-        console.log('❌ Payment failed.');
+        const pi: Stripe.PaymentIntent = data.object as Stripe.PaymentIntent
+        console.log(`🔔  Webhook received: ${pi.object} ${pi.status}!`)
+        console.log('❌ Payment failed.')
     }
   
     if (eventType === 'setup_intent.setup_failed') {
-        console.log(`🔔  A SetupIntent has failed the to setup a PaymentMethod.`);
+        console.log(`🔔  A SetupIntent has failed the to setup a PaymentMethod.`)
     }
   
     if (eventType === 'setup_intent.succeeded') {
         console.log(
           `🔔  A SetupIntent has successfully setup a PaymentMethod for future use.`
-        );
+        )
     }
   
     if (eventType === 'setup_intent.created') {
-        const setupIntent: Stripe.SetupIntent = data.object as Stripe.SetupIntent;
-        console.log(`🔔  A new SetupIntent is created. ${setupIntent.id}`);
+        const setupIntent: Stripe.SetupIntent = data.object as Stripe.SetupIntent
+        console.log(`🔔  A new SetupIntent is created. ${setupIntent.id}`)
     }
   
     res.sendStatus(200);
@@ -154,5 +155,5 @@ function getStripe() {
     return new Stripe(stripeSecretKey as string, {
       apiVersion: '2020-08-27',
       typescript: true,
-    });
+    })
 }
