552f85b66545ac1fa1b6974ad33818e4c409ad09
tagheir@gmail.com <Hisham Asghar>
2023-12-27 11:18:24 +0000

    126 payment pages responsive (#146)
    * Add note about #133
    
    * Responsive PaymentPage using ShoppingCartItemsBuilder
    
    * Cleanup ShoppingCartScreen

--- lib/src/features/checkout/payment/payment_page.dart
+++ lib/src/features/checkout/payment/payment_page.dart
@@ -1,63 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:my_shop_ecommerce_flutter/src/common_widgets/async_value_widget.dart';
-import 'package:my_shop_ecommerce_flutter/src/common_widgets/cart_total_with_cta.dart';
-import 'package:my_shop_ecommerce_flutter/src/common_widgets/decorated_box_with_shadow.dart';
-import 'package:my_shop_ecommerce_flutter/src/constants/app_sizes.dart';
 import 'package:my_shop_ecommerce_flutter/src/features/checkout/payment/payment_button.dart';
 import 'package:my_shop_ecommerce_flutter/src/features/shopping_cart/shopping_cart_item.dart';
+import 'package:my_shop_ecommerce_flutter/src/features/shopping_cart/shopping_cart_items_builder.dart';
 import 'package:my_shop_ecommerce_flutter/src/repositories/database/cart/item.dart';
 import 'package:my_shop_ecommerce_flutter/src/services/cart_service.dart';
 
 class PaymentPage extends ConsumerWidget {
-  PaymentPage({Key? key}) : super(key: key);
-
-  // Adding an explicit ScrollController as a fix for:
-  // > The provided ScrollController is currently attached to more than one ScrollPosition
-  final _scrollController = ScrollController();
+  const PaymentPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final itemsValue = ref.watch(cartItemsListProvider);
     return AsyncValueWidget<List<Item>>(
       value: itemsValue,
-      data: (items) => Column(
-        children: [
-          Expanded(
-            // TODO: Should this be ScrollablePage?
-            child: CustomScrollView(
-              controller: _scrollController,
-              slivers: [
-                const SliverToBoxAdapter(
-                  child: SizedBox(height: Sizes.p16),
-                ),
-                SliverPadding(
-                  padding: const EdgeInsets.all(Sizes.p16),
-                  sliver: SliverList(
-                    delegate: SliverChildBuilderDelegate(
-                      (context, index) {
-                        final item = items[index];
-                        return ShoppingCartItem(
-                          item: item,
-                          itemIndex: index,
-                          // make item non editable so that user can't empty cart completely
-                          isEditable: false,
-                        );
-                      },
-                      childCount: items.length,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          // TODO: Test this on desktop
-          DecoratedBoxWithShadow(
-            child: CartTotalWithCTA(
-              ctaBuilder: (_) => const PaymentButton(),
-            ),
-          )
-        ],
+      data: (items) => ShoppingCartItemsBuilder(
+        items: items,
+        itemBuilder: (_, item, index) => ShoppingCartItem(
+          item: item,
+          itemIndex: index,
+          isEditable: false,
+        ),
+        ctaBuilder: (_) => const PaymentButton(),
       ),
     );
   }
--- lib/src/features/shopping_cart/shopping_cart.dart
+++ lib/src/features/shopping_cart/shopping_cart.dart
@@ -2,11 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:my_shop_ecommerce_flutter/src/common_widgets/async_value_widget.dart';
-import 'package:my_shop_ecommerce_flutter/src/common_widgets/cart_total_with_cta.dart';
-import 'package:my_shop_ecommerce_flutter/src/common_widgets/decorated_box_with_shadow.dart';
 import 'package:my_shop_ecommerce_flutter/src/common_widgets/primary_button.dart';
-import 'package:my_shop_ecommerce_flutter/src/constants/app_sizes.dart';
 import 'package:my_shop_ecommerce_flutter/src/features/shopping_cart/shopping_cart_item.dart';
+import 'package:my_shop_ecommerce_flutter/src/features/shopping_cart/shopping_cart_items_builder.dart';
 import 'package:my_shop_ecommerce_flutter/src/repositories/database/cart/item.dart';
 import 'package:my_shop_ecommerce_flutter/src/routing/app_router.dart';
 import 'package:my_shop_ecommerce_flutter/src/services/cart_service.dart';
@@ -23,116 +21,17 @@ class ShoppingCartScreen extends ConsumerWidget {
       ),
       body: AsyncValueWidget<List<Item>>(
         value: itemsValue,
-        data: (items) => ShoppingCartContents(items: items),
-      ),
-    );
-  }
-}
-
-class ShoppingCartContents extends ConsumerWidget {
-  const ShoppingCartContents({Key? key, required this.items}) : super(key: key);
-  final List<Item> items;
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    if (items.isEmpty) {
-      return Center(
-        child: Text(
-          'Shopping Cart is empty',
-          style: Theme.of(context).textTheme.headline3,
-          textAlign: TextAlign.center,
-        ),
-      );
-    }
-    final screenWidth = MediaQuery.of(context).size.width;
-    // wide layouts
-    if (screenWidth >= FormFactor.tablet) {
-      return Center(
-        child: SizedBox(
-          width: FormFactor.desktop,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: Sizes.p16),
-            child: Row(
-              children: [
-                Flexible(
-                  flex: 3,
-                  // TODO: Hide scrollbar
-                  child: CustomScrollView(
-                    slivers: [
-                      SliverPadding(
-                        padding:
-                            const EdgeInsets.symmetric(vertical: Sizes.p16),
-                        sliver: SliverList(
-                          delegate: SliverChildBuilderDelegate(
-                            (context, index) {
-                              final item = items[index];
-                              return ShoppingCartItem(
-                                item: item,
-                                itemIndex: index,
-                              );
-                            },
-                            childCount: items.length,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                const SizedBox(width: Sizes.p16),
-                const Flexible(
-                  flex: 1,
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(vertical: Sizes.p16),
-                    child: ShoppingCartCheckout(),
-                  ),
-                )
-              ],
-            ),
+        data: (items) => ShoppingCartItemsBuilder(
+          items: items,
+          itemBuilder: (_, item, index) => ShoppingCartItem(
+            item: item,
+            itemIndex: index,
+          ),
+          ctaBuilder: (_) => PrimaryButton(
+            text: 'Checkout',
+            onPressed: () => context.goNamed(AppRoute.checkout.name),
           ),
         ),
-      );
-    } else {
-      // narrow layouts
-      return Column(
-        children: [
-          Expanded(
-            child: CustomScrollView(
-              slivers: [
-                SliverPadding(
-                  padding: const EdgeInsets.all(Sizes.p16),
-                  sliver: SliverList(
-                    delegate: SliverChildBuilderDelegate(
-                      (context, index) {
-                        final item = items[index];
-                        return ShoppingCartItem(
-                          item: item,
-                          itemIndex: index,
-                        );
-                      },
-                      childCount: items.length,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          const DecoratedBoxWithShadow(
-            child: ShoppingCartCheckout(),
-          ),
-        ],
-      );
-    }
-  }
-}
-
-class ShoppingCartCheckout extends ConsumerWidget {
-  const ShoppingCartCheckout({Key? key}) : super(key: key);
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return CartTotalWithCTA(
-      ctaBuilder: (_) => PrimaryButton(
-        text: 'Checkout',
-        onPressed: () => context.pushNamed(AppRoute.checkout.name),
       ),
     );
   }
