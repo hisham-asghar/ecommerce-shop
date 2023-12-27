3d3ec4a687533952bcadeb0d090a57e1243c6bce
tagheir@gmail.com <Hisham Asghar>
2023-12-27 11:24:01 +0000

    Tweak not found page

--- lib/src/features/not_found/not_found_screen.dart
+++ lib/src/features/not_found/not_found_screen.dart
@@ -7,28 +7,30 @@ import 'package:my_shop_ecommerce_flutter/src/routing/app_router.dart';
 
 class NotFoundScreen extends ConsumerWidget {
   const NotFoundScreen({Key? key}) : super(key: key);
-  // TODO: Customise error?
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(),
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              '404 - Page not found!',
-              style: Theme.of(context).textTheme.headline3,
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: Sizes.p32),
-            PrimaryButton(
-              onPressed: () => context.goNamed(AppRoute.home.name),
-              text: 'Go Home',
-            )
-          ],
+      body: Padding(
+        padding: const EdgeInsets.all(Sizes.p16),
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                '404 - Page not found!',
+                style: Theme.of(context).textTheme.headline4,
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: Sizes.p32),
+              PrimaryButton(
+                onPressed: () => context.goNamed(AppRoute.home.name),
+                text: 'Go Home',
+              )
+            ],
+          ),
         ),
       ),
     );
