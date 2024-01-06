060120bd1f7ed3c2b88f937d2811af1774085227
tagheir@gmail.com <Hisham Asghar>
2024-01-06 11:25:48 +0000

    Cleanup imports

--- lib/src/features/product_list/products_grid.dart
+++ lib/src/features/product_list/products_grid.dart
@@ -1,15 +1,14 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_layout_grid/flutter_layout_grid.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:my_shop_ecommerce_flutter/src/common_widgets/async_value_widget.dart';
 import 'package:my_shop_ecommerce_flutter/src/constants/app_sizes.dart';
 import 'package:my_shop_ecommerce_flutter/src/features/product_list/product_card.dart';
 import 'package:my_shop_ecommerce_flutter/src/repositories/database/products/product.dart';
-import 'package:my_shop_ecommerce_flutter/src/routing/app_router.dart';
 import 'package:my_shop_ecommerce_flutter/src/services/products_search_service.dart';
-import 'package:flutter_layout_grid/flutter_layout_grid.dart';
 
 class ProductsGrid extends ConsumerWidget {
   const ProductsGrid({Key? key, required this.productSelectedRoute})
