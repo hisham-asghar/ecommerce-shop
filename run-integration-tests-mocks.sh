01b95699971714ff5bb3da976acf82d3471acf2b
tagheir@gmail.com <Hisham Asghar>
2024-02-02 09:06:31 +0000

    Update script to run integration tests

--- run-integration-tests-mocks.sh
+++ run-integration-tests-mocks.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-flutter drive --driver=test_driver/integration_test.dart --target=integration_test/purchase_flow_test.dart --flavor=development
+flutter test integration_test/purchase_flow_test.dart
