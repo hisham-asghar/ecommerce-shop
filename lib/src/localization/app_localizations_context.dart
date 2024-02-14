aae5af6417f9a75d7452d21bac0156845886b27d
tagheir@gmail.com <Hisham Asghar>
2024-02-14 09:25:32 +0000

    Add note about Restart Analysis Server

--- lib/src/localization/app_localizations_context.dart
+++ lib/src/localization/app_localizations_context.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/widgets.dart';
+// * If this import doesn't work, run this:
+// * View > Command Palette > Dart: Restart Analysis Server.
+// * More info here: https://stackoverflow.com/questions/64574620/target-of-uri-doesnt-exist-packageflutter-gen-gen-l10n-gallery-localizations
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 extension LocalizedBuildContext on BuildContext {
