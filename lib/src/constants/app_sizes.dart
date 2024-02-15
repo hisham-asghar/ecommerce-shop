e44fb9e9d07eecd326748e831f77bfd6d8198338
tagheir@gmail.com <Hisham Asghar>
2024-02-15 14:56:24 +0000

    Documentation

--- lib/src/constants/app_sizes.dart
+++ lib/src/constants/app_sizes.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+/// Constant sizes to be used in the app (paddings, gaps, rounded corners etc.)
 class Sizes {
   static const p4 = 4.0;
   static const p8 = 8.0;
@@ -12,11 +13,13 @@ class Sizes {
   static const p64 = 64.0;
 }
 
+/// Layout breakpoints used in the app.
 class Breakpoint {
   static const double desktop = 900;
   static const double tablet = 600;
 }
 
+/// Constant gap widths
 const gapW4 = SizedBox(width: Sizes.p4);
 const gapW8 = SizedBox(width: Sizes.p8);
 const gapW12 = SizedBox(width: Sizes.p12);
@@ -27,6 +30,7 @@ const gapW32 = SizedBox(width: Sizes.p32);
 const gapW48 = SizedBox(width: Sizes.p48);
 const gapW64 = SizedBox(width: Sizes.p64);
 
+/// Constant gap heights
 const gapH4 = SizedBox(height: Sizes.p4);
 const gapH8 = SizedBox(height: Sizes.p8);
 const gapH12 = SizedBox(height: Sizes.p12);
