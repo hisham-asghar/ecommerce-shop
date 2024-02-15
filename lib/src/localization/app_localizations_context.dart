4c1e339a45181e1bf44004b3da5bc0e777890bc9
tagheir@gmail.com <Hisham Asghar>
2024-02-15 11:04:08 +0000

    Document LocalizedBuildContext

--- lib/src/localization/app_localizations_context.dart
+++ lib/src/localization/app_localizations_context.dart
@@ -4,6 +4,13 @@ import 'package:flutter/widgets.dart';
 // * More info here: https://stackoverflow.com/questions/64574620/target-of-uri-doesnt-exist-packageflutter-gen-gen-l10n-gallery-localizations
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
+/// Helper extension that allows to type this:
+/// Text(context.loc.someString)
+/// rather than this:
+/// Text(AppLocalizations.of(context).someString)
+///
+/// More info here:
+/// https://codewithandrea.com/articles/flutter-localization-build-context-extension/
 extension LocalizedBuildContext on BuildContext {
   AppLocalizations get loc => AppLocalizations.of(this);
 }
